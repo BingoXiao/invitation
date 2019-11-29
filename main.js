ad434ae5de5094e831cbedeb062372b1d6e3d465
xiao <11091402@qq.com>
2019-11-29 08:44:21 +0800

    ios formdata方法限制

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(function () {
+    /*签到*/
     $("#SignIn").click(function () {
         var _this = this;
         /*
@@ -13,7 +14,8 @@ $(function () {
         } else {
             $(_this).attr("disabled", "disabled");
 
-            var fields = $("form").serializeArray();
+            var fields = $("#sign_in_form input, select").serializeArray();
+
             var datas = {};
             $.each(fields, function (index, item) {
                 //文本表单的值不为空才处理
@@ -29,17 +31,21 @@ $(function () {
                 $(_this).removeAttr("disabled");
             } else {
                 var formdata = new FormData();
-                formdata.set('Jjcode', $('#Jjcode').val());
-                formdata.set('ts', new Date().getTime());
-                for(var key in datas) {
-                    formdata.set(key, datas[key]);
+                formdata.append('Jjcode', $('#Jjcode').val());
+                formdata.append('ts', new Date().getTime());
+                for (var key in datas) {
+                    if (key == 'Jjcode') {
+                        formdata.set(key, datas[key]);
+                    } else {
+                        formdata.append(key, datas[key]);
+                    }
                 }
                 /*
                  * 403:签到过了
                  * */
                 $.ajax({
                     type: "POST",
-                    url: "http://192.168.124.16:3099/v1/activetmp/sign",
+                    url: "https://active.0717aier.com/v1/activetmp/sign",
                     /*
                     * 查询字符串，比如 key1=value1&amp;key2=value2 ，
                     * 映射，比如 {key1: 'value1', key2: 'value2'} 。
@@ -60,59 +66,10 @@ $(function () {
                 });
             }
         }
-
-        /*测试专用*/
-        /*$(_this).attr("disabled", "disabled");
-
-        var fields = $("form").serializeArray();
-        var datas = {};
-        $.each(fields, function (index, item) {
-            //文本表单的值不为空才处理
-            if (item.name != 'Jjcode') {
-                if (item.value && $.trim(item.value) != "") {
-                    datas[item.name] = item.value;
-                }
-            }
-        });
-
-        if (Object.keys(datas).length != 7) {
-            alert("请填写完整签到信息！");
-            $(_this).removeAttr("disabled");
-        } else {
-            var formdata = new FormData();
-            formdata.set('Jjcode', $('#Jjcode').val());
-            formdata.set('ts', new Date().getTime());
-            for(var key in datas) {
-                formdata.set(key, datas[key]);
-            }
-            /!*datas.Jjcode = $('#Jjcode').val();
-            datas.ts = new Date().getTime();
-            console.log(datas);*!/
-            /!*
-             * 403:签到过了
-             * *!/
-            $.ajax({
-                type: "POST",
-                url: "http://192.168.124.16:3099/v1/activetmp/sign",
-                /!*
-                * 查询字符串，比如 key1=value1&amp;key2=value2 ，
-                * 映射，比如 {key1: 'value1', key2: 'value2'} 。
-                * 如果使用了后者的形式，则数据再发送器会被转换成查询字符串。设置processData选项为false来回避*!/
-                data: formdata,
-                contentType: false,
-                processData: false,
-                success: function (msg) {
-                    window.location.href = './success.html';
-                },
-                error: function (err) {
-                    alert(err.status);
-                    $(_this).removeAttr("disabled");
-                    console.log(err)
-                }
-            });
-        }*/
     });
 
+
+    /*下载图片*/
     $("#chartBtn").click(function () {
         window.location.href = './chart.html';
     });
